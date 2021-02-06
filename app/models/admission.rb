17b6bb0aea4ff86d4279b04f6b933be3f66756d2
David Admin <davidadmin@Davids-iMac.local>
2021-02-06 17:53:07 -0500

    adds date_time_format and description_format modules to admission model

--- app/models/admission.rb
+++ app/models/admission.rb
@@ -1,8 +1,18 @@
+require 'date_time_format'
+require 'description_format'
+
 class Admission < ApplicationRecord
+  include DateTimeFormat
+  include DescriptionFormat
+
   has_many :diagnoses
   has_many :symptoms
   has_many :observations
   belongs_to :patient
   belongs_to :facility
   alias_attribute :moment, :created_at
+
+  def diagnosis_description
+    formatted_description(diagnoses)
+  end
 end
--- spec/models/admission_spec.rb
+++ spec/models/admission_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Admission, type: :model do
+  before do
+    @admission = create :admission, moment: DateTime.new(2021,2,6,11)
+  end
+
   describe "associations" do
     it { should have_many :diagnoses }
     it { should have_many :symptoms }
@@ -11,8 +15,35 @@ RSpec.describe Admission, type: :model do
 
   describe "moment" do
     it "should have the same datetime value as admission's created_at attribute" do
-      admission = create :admission
-      assert_equal admission.moment, admission.created_at
+      assert_equal @admission.moment, @admission.created_at
+    end
+  end
+
+  describe "formatted_date" do
+    it "should neatly display the date of the patient's admission" do
+      expect(@admission.formatted_date).to eql "February 6, 2021"
+    end
+  end
+
+  describe "formatted_time" do
+    it "should neatly display the time of the patient's admission" do
+      expect(@admission.formatted_time).to eql "11:00 am"
+    end
+  end
+
+  describe 'diagnosis_description' do
+    before do
+      @admission.diagnoses << create(:diagnosis, description: "femoral shaft (S95.103A)")
+      @admission.diagnoses << create(:diagnosis, description: "femur (S95.105C)")
+    end
+
+    it "should clearly describe an individual diagnosis" do
+      @admission.diagnoses.destroy @admission.diagnoses.last
+      expect(@admission.diagnosis_description).to eql "femoral shaft (S95.103A)"
+    end
+
+    it "should clearly describe the patient's diagnoses" do
+      expect(@admission.diagnosis_description).to eql "femoral shaft (S95.103A) and femur (S95.105C)"
     end
   end
 end
