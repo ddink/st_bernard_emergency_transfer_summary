bb78fc27f3b9b7d340565a874600e125df16471d
David Admin <davidadmin@Davids-iMac.local>
2021-02-06 17:51:06 -0500

    adds date_time_format module to diagnostic procedure model

--- app/models/diagnostic_procedure.rb
+++ app/models/diagnostic_procedure.rb
@@ -1,4 +1,7 @@
+require 'date_time_format'
+
 class DiagnosticProcedure < ApplicationRecord
+  include DateTimeFormat
   validates_presence_of :description
   alias_attribute :moment, :created_at
 end
--- spec/models/diagnostic_procedure_spec.rb
+++ spec/models/diagnostic_procedure_spec.rb
@@ -1,14 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe DiagnosticProcedure, type: :model do
+  before do
+    @diagnostic_procedure = create :diagnostic_procedure, created_at: DateTime.new(2021,2,6,11)
+  end
+
   describe 'validations' do
     it { should validate_presence_of :description }
   end
 
   describe "moment" do
     it "should have the same datetime value as diagnostic procedure's created_at attribute" do
-      diagnostic_procedure = create :diagnostic_procedure
-      assert_equal diagnostic_procedure.moment, diagnostic_procedure.created_at
+      assert_equal @diagnostic_procedure.moment, @diagnostic_procedure.created_at
+    end
+  end
+
+  describe "formatted_date" do
+    it "should neatly display the date of the patient's admission" do
+      expect(@diagnostic_procedure.formatted_date).to eql "February 6, 2021"
+    end
+  end
+
+  describe "formatted_time" do
+    it "should neatly display the time of the patient's admission" do
+      expect(@diagnostic_procedure.formatted_time).to eql "11:00 am"
     end
   end
 end
